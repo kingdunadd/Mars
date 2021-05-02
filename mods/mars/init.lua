fe6802994ca9ed24c494e7a217850577ec086559
Daisy <daisy@perlucida.uk>
2021-05-02 14:31:49 +0100

    Changed textures and added more

--- mods/mars/init.lua
+++ mods/mars/init.lua
@@ -14,12 +14,30 @@
 --oddly_breakable_by_hand	any	               Torches and such - very quick to dig
 
 
+
 minetest.register_node("mars:stone", {
 	description = "Mars Stone",
 	tiles = {"mars_stone.png"},
 	is_ground_content = true,
-	groups = {cracky=3, stone=1}
+	groups = {cracky=3, stone=1},
+    drop = "mars:cobblestone"
 })
+
+minetest.register_node("mars:stair_stone", {
+    description = "Mars Stone Stair",
+    tiles = {"mars_stone.png"},
+    groups = {cracky=3, stone=1},
+    drawtype = "nodebox",
+    paramtype = "light",
+    node_box = {
+        type = "fixed",
+        fixed = {
+            {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+            {-0.5, 0, 0, 0.5, 0.5, 0.5},
+        },
+    }
+})
+
 
 minetest.register_node("mars:cobblestone", {
 	description = "Mars Cobblestone",
@@ -28,12 +46,65 @@ minetest.register_node("mars:cobblestone", {
 	groups = {cracky=3, stone=1}
 })
 
+minetest.register_node("mars:stair_cobblestone", {
+    description = "Mars Cobblestone Stair",
+    tiles = {"mars_cobblestone.png"},
+    drawtype = "nodebox",
+    paramtype = "light",
+    groups = {cracky=3, stone=1},
+    node_box = {
+        type = "fixed",
+        fixed = {
+            {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+            {-0.5, 0, 0, 0.5, 0.5, 0.5},
+        },
+    }
+})
+
+
+minetest.register_node("mars:stone_brick", {
+	description = "Mars Brick",
+	tiles = {"mars_brick.png"},
+	is_ground_content = true,
+	groups = {cracky=3, stone=1}
+})
+
+minetest.register_node("mars:stair_brick", {
+    description = "Mars Brick Stair",
+    tiles = {"mars_brick.png"},
+    drawtype = "nodebox",
+    paramtype = "light",
+    groups = {cracky=3, stone=1},
+    node_box = {
+        type = "fixed",
+        fixed = {
+            {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+            {-0.5, 0, 0, 0.5, 0.5, 0.5},
+        },
+    }
+})
+
 
 minetest.register_node("mars:sand", {
 	description = "Mars Sand",
 	tiles = {"mars_sand.png"},
 	is_ground_content = true,
 	groups = {crumbly=3}
+})
+
+minetest.register_node("mars:stair_sand", {
+    description = "Mars Sand Stair",
+    tiles = {"mars_sand.png"},
+    drawtype = "nodebox",
+    paramtype = "light",
+    groups = {crumbly=3},
+    node_box = {
+        type = "fixed",
+        fixed = {
+            {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+            {-0.5, 0, 0, 0.5, 0.5, 0.5},
+        },
+    }
 })
 
 
@@ -49,17 +120,17 @@ minetest.register_node("mars:glass", {
 })
 
 
-minetest.register_biome({
-    name = "mars_stone",
-    node_top = "mars:stone",
-    depth_top = 1,
-    node_filler = "default:stone",
-    depth_filler = 3,
-    y_max = 1000,
-    y_min = -3,
-    heat_point = 50,
-    humidity_point = 50,
-})
+--minetest.register_biome({
+    --name = "mars_stone",
+    --node_top = "mars:stone",
+    --depth_top = 1,
+    --node_filler = "default:stone",
+    --depth_filler = 3,
+    --y_max = 1000,
+    --y_min = -3,
+    --heat_point = 50,
+    --humidity_point = 50,
+--})
 
 minetest.register_biome({
     name = "mars_sand",
@@ -74,4 +145,6 @@ minetest.register_biome({
 })
 
 minetest.register_alias("mapgen_stone", "mars:stone")
-
+minetest.register_alias("mapgen_water_source", "mars:sand")
+minetest.register_alias("default:cobble", "mars:stone")
+itemname = minetest.registered_aliases[itemname] or itemname
