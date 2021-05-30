6d36ea56e54a124853b24dcb8302a6ef445dd117
Daisy <daisy@perlucida.uk>
2021-05-30 22:12:40 +0100

    Added player api and texture

--- mods/mars/player.lua
+++ mods/mars/player.lua
@@ -1,9 +1,9 @@
 
 minetest.register_on_joinplayer(function(player)
     if player then
-        --local player = minetest.get_player_by_name("singleplayer") or minetest.get_player_by_name(name)
         player:set_physics_override({
             gravity = 0.38, -- mars gravity is 3.711ms-1 or approx 38% of earth
         })
+        player:set_sky({r=191, g=186, b=167}, "plain")
     end
 end)
